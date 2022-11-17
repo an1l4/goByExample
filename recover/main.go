a371c950e4c5703811ec228dc1b94c5cddae8106
an1l4 <1995anila@gmail.com>
2022-11-17 16:42:31 +0530

    added-files

---
+++ recover/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func mayPanic() {
+	panic("a problem")
+}
+
+func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recovered error\n", r)
+		}
+
+	}()
+
+	mayPanic()
+
+	fmt.Println("after mayPanic()")
+
+}
